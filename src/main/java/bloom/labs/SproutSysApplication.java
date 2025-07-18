b02c0b605934c7845ed3346209ce1c210ff8961c
Cpimentel <frpimentel00@gmail.com>
2025-07-17 22:16:24 -0300

    subindo escopo inicial do projeto

---
+++ src/main/java/bloom/labs/SproutSysApplication.java
@@ -0,0 +1,13 @@
+package bloom.labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproutSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bloom/labs/SproutSysApplicationTests.java
@@ -0,0 +1,13 @@
+package bloom.labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproutSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
