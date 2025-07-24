f1710b726ac29f66be4cbf2db49925cbb9f8c739
Cpimentel <frpimentel00@gmail.com>
2025-07-23 23:16:27 -0300

    adicionando o flymigration da tabela produto

--- src/main/resources/db/migration/Create_table_produtos.sql
+++ src/main/resources/db/migration/Create_table_produtos.sql
@@ -2,7 +2,7 @@ CREATE TABLE produtos (
     id SERIAL PRIMARY KEY,
     descricao VARCHAR(255) NOT NULL,
     categoria_id INT NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
+    preco DECIMAL NOT NULL,
     quantidade_estoque INT NOT NULL,
     unidade_medida VARCHAR(20) NOT NULL,
     codigo_barras VARCHAR(50) UNIQUE NOT NULL,
